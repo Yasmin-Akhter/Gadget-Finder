5994feef2aae3de04b5ad78ca0f0ec9cc71ed8d9
Yasmin Akhter <yasminbd89@yahoo.com>
2022-03-01 19:23:33 +0600

    2nd commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('ok');
+const loadPhone = () => {
+    const searchText = document.getElementById('search-input').value;
+
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showPhone(data.data))
+}
+const showPhone = name => {
+    console.log(name);
+
+}
