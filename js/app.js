7268ef826b1b04fcd6e2b906dfef3438efc6cf1a
Yasmin Akhter <yasminbd89@yahoo.com>
2022-03-01 21:14:34 +0600

    details added

--- js/app.js
+++ js/app.js
@@ -38,17 +38,20 @@ const showDetails = (id) => {
 
 }
 const showDetailsInner = items => {
+    console.log(items);
     const details = document.getElementById('details');
     const div = document.createElement('div');
     div.innerHTML = `
         <div class="card" style="width: 18rem;">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="${items.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">${items.name}</h5>
+                    <h5 class="card-text">Release Date:${items.releaseDate}</h5>
+                    <h5 class="card-text">Release Date:${items.releaseDate}</h5>
+                    
                 </div>
         </div>
         `;
+    details.appendChild(div);
 }
 
